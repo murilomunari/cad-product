861bde8c18dbde686862108046845af7b4ef7908
Murilo <munarimurilo441@gmail.com>
2024-04-23 21:12:17 -0300

    Methods POST and DELETE created

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,14 +1,40 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.DTO.ProductDTO;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductDTO productDTO){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productDTO, productModel); // copyPropertis faz a conversão de DTO para model
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Object> deletePrduct(@PathVariable(value = "id")UUID id, @RequestBody @Valid ProductDTO productDTO) {
+         Optional<ProductModel> product0= productRepository.findById(id);
+         if (product0.isEmpty()) {
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+         }
+         productRepository.delete(product0.get());
+         return  ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
 
 }
--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode()
+@ToString
 public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
