248ebc9e7285b179fcf9b89bdbaee342db72c395
Murilo <munarimurilo441@gmail.com>
2024-04-23 22:47:32 -0300

    Method Put created and method delete updated

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -27,14 +27,25 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id")UUID id, @RequestBody @Valid ProductDTO productDTO) {
          Optional<ProductModel> product0= productRepository.findById(id);
          if (product0.isEmpty()) {
-             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
          }
          productRepository.delete(product0.get());
          return  ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductDTO productDTO){
+        Optional<ProductModel> product0= productRepository.findById(id);
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        var productModel = product0.get();
+        BeanUtils.copyProperties(productDTO, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
 }
